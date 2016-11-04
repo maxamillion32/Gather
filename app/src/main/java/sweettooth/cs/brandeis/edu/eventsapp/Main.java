68c11d6a697f59b05a0d90967ca6ee21bc4f5235
Tyler Lichten <tlich10@gmail.com>
2016-11-04 13:26:08 -0400

    test

--- app/src/main/java/sweettooth/cs/brandeis/edu/eventsapp/Main.java
+++ app/src/main/java/sweettooth/cs/brandeis/edu/eventsapp/Main.java
@@ -10,4 +10,6 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //tyler testing VCS in Android Studio--delete me!
 }
