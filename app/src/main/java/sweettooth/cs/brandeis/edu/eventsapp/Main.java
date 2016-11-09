212f04e080fb419db57c8bb1fa2e40b8bacceec5
Christine Kim <kkej0630@gmail.com>
2016-11-08 21:31:08 -0500

    implement three bottom tabs

--- app/src/main/java/sweettooth/cs/brandeis/edu/eventsapp/Main.java
+++ app/src/main/java/sweettooth/cs/brandeis/edu/eventsapp/Main.java
@@ -1,13 +1,43 @@
 package sweettooth.cs.brandeis.edu.eventsapp;
 
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabSelectedListener;
+
 public class Main extends AppCompatActivity {
+
+    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.three_buttons_activity);
+
+        BottomBar bottomBar = BottomBar.attach(this, savedInstanceState);
+        bottomBar.setItemsFromMenu(R.menu.bottom_menu, new OnMenuTabSelectedListener() {
+            @Override
+            public void onMenuItemSelected(int itemId) {
+                switch (itemId) {
+                    case R.id.home_item:
+                        Snackbar.make(coordinatorLayout, "Home Selected", Snackbar.LENGTH_LONG).show();
+                        break;
+                    case R.id.explore_item:
+                        Snackbar.make(coordinatorLayout, "Explore Selected", Snackbar.LENGTH_LONG).show();
+                        break;
+                    case R.id.settings_item:
+                        Snackbar.make(coordinatorLayout, "Settings Selected", Snackbar.LENGTH_LONG).show();
+                        break;
+                }
+            }
+        });
+
+        // Set the color for the active tab. Ignored on mobile when there are more than three tabs.
+        bottomBar.setActiveTabColor("#C2185B");
     }
 }
